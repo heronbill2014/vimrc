f6eef81a4bc37d3cd2bfa1055c959f60fb58f082
syy <yuanyuan@onescorpion.com>
2018-07-30 09:46:14 +0800

    添加autocomplpop页面内自动补全插件

--- myvim-tool/README.md
+++ myvim-tool/README.md
@@ -2,7 +2,7 @@
 ## 此工具用于构建c/c++开发环境
 ## 安装内容
 	安装ctags，cscope
-	安装winmanager，omni自动补全插件
+	安装winmanager，omni和autocomplpop自动补全插件,
 	覆盖.vimrc文件
 	reset_index.sh 用于生成ctags和cscope索引文件
 ## 用法
--- myvim-tool/install_vimrc.sh
+++ myvim-tool/install_vimrc.sh
@@ -5,6 +5,8 @@ rpm -ivh ./achive/ctags-5.8-13.el7.x86_64.rpm
 
 unzip ./achive/omnicppcomplete-0.41.zip -d ~/.vim
 unzip ./achive/winmanager.zip -d ~/.vim 
+unzip ./achive/vim-autocomplpop.zip -d ~/.vim 
+
 cp -f ./achive/vimrc ~/.vimrc
 cp -f ./achive/reset_index.sh ~/
 
